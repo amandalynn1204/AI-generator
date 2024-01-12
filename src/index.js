23313b35279808eb67061e8660f851b5ae723ece
amandalynn1204 <amandalynn1204@icloud.com>
2024-01-12 14:20:30 -0500

    improved UI

--- src/index.js
+++ src/index.js
@@ -9,16 +9,18 @@ function displayPoem(response) {
 
 function generatePoem(event) {
   event.preventDefault();
-  poemElement.classList.add("poem");
 
   const keywordInput = document.querySelector("#keyword-input");
   const apiKey = "9a7ca83bt1f54ebc3o8f9d804f5e2b0e";
   const context =
-    "You are a poetic and romantic AI assistant. Your job is to generate a four line poem, using HTML format. Make sure to seperate every line with a <br />, but do not add a <br /> at the end of the poem. Use proper grammar. Do not use italized or bold text.";
+    "You are a poetic and romantic AI assistant. Your job is to generate a four line poem, using HTML format. Make sure to seperate every line with a <br />, but do NOT add a <br /> at the end of the poem. Use proper grammar. Do not use italized or bold text.";
   let prompt = `Generate a poem about ${keywordInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayPoem);
+
+  poemElement.classList.add("poem");
+  poemElement.innerHTML = `<strong class="generating" >⏳ Generating a poem about ${keywordInput.value}</strong>`;
 }
 
 const poemFormElement = document.querySelector("form");
