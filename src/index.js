139011f3edad361b9dbcb2660a62e8f3793555b9
amandalynn1204 <amandalynn1204@icloud.com>
2024-01-12 13:09:48 -0500

    added form handling and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generatePoem(event) {
+  event.preventDefault();
+  poemElement.classList.add("poem");
+  new Typewriter(poemElement, {
+    strings: "Loveliest of trees, the cherry now",
+    autoStart: true,
+    cursor: "",
+    delay: 20,
+  });
+}
 
+const poemFormElement = document.querySelector("form");
+const poemElement = document.querySelector("#poem");
+
+poemFormElement.addEventListener("submit", generatePoem);
